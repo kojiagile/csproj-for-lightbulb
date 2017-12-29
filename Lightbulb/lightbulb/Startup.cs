5e46fb70523ade9957b6f7708f63796f8450a84d
koji nishimoto <koji.nishimoto@orica.com>
2017-12-29 16:54:06 +1100

    fix bug

--- Lightbulb/lightbulb/Startup.cs
+++ Lightbulb/lightbulb/Startup.cs
@@ -19,9 +19,7 @@ namespace lightbulb
 
             // comment
         }
-
-        a
-
+        
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
